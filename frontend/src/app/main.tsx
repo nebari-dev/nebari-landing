0aeecab07bcbcbd2079f94a67077cf991144fa53
Johnny Bouder <jbouder@openteams.com>
2026-03-27 14:35:44 -0400

    Include favicon customization.

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -23,6 +23,12 @@ const appConfig = getAppConfig();
 if (appConfig?.title) {
   document.title = appConfig.title;
 }
+if (appConfig?.faviconUrl) {
+  const link = (document.querySelector("link[rel~='icon']") ??
+    Object.assign(document.createElement("link"), { rel: "icon" })) as HTMLLinkElement;
+  link.href = appConfig.faviconUrl;
+  document.head.appendChild(link);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
